ab469800078eb886c03b53c34d8c9cace6d728b4
Joab-S <joabsilva2002@gmail.com>
2024-06-08 15:30:14 -0300

    adjust config.py - redis verification

--- app/core/config.py
+++ app/core/config.py
@@ -5,4 +5,11 @@ redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_port = int(os.getenv("REDIS_PORT", 6379))
 expiration_time = int(os.getenv("EXPIRATION_TIME", 604800))
 
-redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0, decode_responses=True)
+try:
+    redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0, decode_responses=True)
+    
+    # Verificar a conexão com o Redis
+    redis_client.ping()
+    print("Conexão com o Redis estabelecida com sucesso!")
+except redis.exceptions.ConnectionError as e:
+    print(f"Erro ao conectar ao Redis: {e}")
