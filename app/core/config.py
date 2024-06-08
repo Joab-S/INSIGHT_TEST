a9fa68b700d13dde4a094cbe486c63feb7b08793
Joab-S <joabsilva2002@gmail.com>
2024-06-08 16:39:15 -0300

    + adjusts redis: remove redis-server

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,8 @@
 import redis
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_port = int(os.getenv("REDIS_PORT", 6379))
--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
 import requests_cache
 from app.api.endpoints.urls import api_router
-from app.core.redis_server import start_redis_server
 
 app = FastAPI(
     title = "API de Municípios do Ceará",
     description = "API útil para fornecer informações sobre os municípios do Ceará para um mapa iterativo.",
     version = "1.0.0"
 )
-
-start_redis_server()
 
 requests_cache.install_cache(cache_name='insight_cache', backend='sqlite', expire_after=180)
 app.include_router(api_router)
